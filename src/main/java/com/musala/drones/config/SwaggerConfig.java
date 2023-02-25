b4293ba22742c2b90fcd5a50339142a293763d21
Mohamed Fayez <Mohamed.fayez@mdi.com>
2023-02-25 10:54:57 +0200

    readme step by step

--- src/main/java/com/musala/drones/config/SwaggerConfig.java
+++ src/main/java/com/musala/drones/config/SwaggerConfig.java
@@ -14,12 +14,12 @@ public class SwaggerConfig {
 	@Bean
 	  public OpenAPI openAPI() {
 	      return new OpenAPI()
-	              .info(new Info().title("Drone Delivery System")
+	              .info(new Info().title("Drones Delivery System")
 	              .description("There is a major new technology that is destined to be a disruptive force in the field of transportation")
 	              .version("1.0.0")
 	              .license(new License().name("Musala Soft 1.0")))
 	              .externalDocs(new ExternalDocumentation()
-	              .description("Drone Delivery System"));
+	              .description("Drones Delivery System"));
 	  }
 
 }
