754471c237a93a73e765908675c6facdc095d384
Mohamed Fayez <Mohamed.fayez@mdi.com>
2023-02-26 08:58:21 +0200

    adding test data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into Drone (serial_number, weight_limit, battery_capacity, model, state,
+created_at, updated_at) VALUES ('000-000', '500', '100', 'HEAVYWEIGHT', 'IDLE', null, null);
+insert into Drone (serial_number, weight_limit, battery_capacity, model, state,
+created_at, updated_at) VALUES ('000-001', '200', '100', 'LIGHTWEIGHT', 'IDLE', null, null);
+insert into Drone (serial_number, weight_limit, battery_capacity, model, state,
+created_at, updated_at) VALUES ('000-002', '400', '100', 'HEAVYWEIGHT', 'IDLE', null, null);
+insert into Drone (serial_number, weight_limit, battery_capacity, model, state,
+created_at, updated_at) VALUES ('000-003', '300', '100', 'LIGHTWEIGHT', 'IDLE', null, null);
